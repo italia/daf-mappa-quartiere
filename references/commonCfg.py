35d6bc3c6cbf48cd323267f39c2d655f61b162ac
Davide <ymn>
2018-04-09 20:15:50 +0200

    introduced absolute paths with respect to the configuration module

--- references/commonCfg.py
+++ references/commonCfg.py
@@ -1,24 +1,32 @@
 import pandas as pd
 import geopandas as gpd
+import os
 
+projRoot = os.path.dirname(os.path.dirname(__file__)) # expected to be in root/references
 
 # cities included in the project
 cityList = ['Milano', 'Torino', 'Roma']
 
 # Istat parameters
+cpaPath = os.path.join(projRoot,'data/raw/istat/dati-cpa_2011/Sezioni di Censimento/')
 sezioneColName = 'SEZ2011'
 IdQuartiereColName = 'IDquartiere'
-
 
 ## Loading tools
 def get_istat_cpa_data(cityName):
     # hardcoded filename standard
-    loaded = gpd.read_file('../data/processed/'+cityName+'_sezioni.geojson')
+    loaded = gpd.read_file(os.path.join(projRoot, 'data/processed/'+cityName+'_sezioni.geojson'))
     assert set([sezioneColName, IdQuartiereColName]) <= set(loaded.columns), \
         'Missing expected standard columns for city %s' % cityName
     return loaded.set_index(sezioneColName)
 
 
+def get_istat_filelist():
+    return [f for f in os.listdir(cpaPath) if f.startswith('R')]
+
+
+cached_metadata = pd.read_csv(os.path.join(cpaPath, 'tracciato_2011_sezioni.csv'), sep=';',encoding='cp1252')
+
 def get_istat_metadata():
-    return pd.read_csv('../data/raw/istat/dati-cpa_2011/Sezioni di Censimento/tracciato_2011_sezioni.csv', sep=';', encoding='cp1252')
+    return cached_metadata
 
