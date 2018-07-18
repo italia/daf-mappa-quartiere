a529798c81cb7d9a6f2d5c647f407c16c107ad72
Davide Veronese <d.v.davideveronese@gmail.com>
2018-07-18 16:55:32 +0200

    include array evaluation in test

--- src/testing/model_tests.py
+++ src/testing/model_tests.py
@@ -11,12 +11,13 @@ mock_service_type = next(ServiceType.__iter__())
 mock_age_group = next(AgeGroup.__iter__())
 lengthscale = 1
 
-def get_sample_unit():
+
+def get_sample_unit(coords=(40, 9)):
     sample_unit = ServiceUnit(
         service=mock_service_type,
         name='test unit',
         unit_id=1,
-        position=geopy.Point(40, 9),
+        position=geopy.Point(*coords),
         capacity=np.nan,
         lengthscales={mock_age_group: lengthscale},
         kernel_thresholds=None,
@@ -37,33 +38,28 @@ class TestServiceUnit(unittest.TestCase):
 
     def test_single_evaluation(self):
         sample_unit = get_sample_unit()
-        for sigma in [1,2,3]:
+        for sigma in [1, 2, 3]:
             step = geopy.distance.great_circle(sigma)
-            for bearing in np.arange(0,360,90): # test
-                target_point = step.destination(
+            points_list = []
+            #target_points = np.zeros_like([0,2])
+            for bearing in np.arange(0, 360, 90):  # test many directions
+                new_point = step.destination(
                     sample_unit.position, bearing=bearing)
-                coords = np.array(
-                    [target_point.latitude, target_point.longitude])
-                interaction = sample_unit.evaluate(
-                    coords[np.newaxis,:], mock_age_group)
+                points_list.append(np.array(
+                    [new_point.latitude, new_point.longitude]))
+            interactions = sample_unit.evaluate(
+                np.stack(points_list), mock_age_group)
 
-                self.assertAlmostEquals(np.exp(- sigma ** 2 / 2), interaction)
-
-
-
-
+            for computed in interactions:
+                self.assertAlmostEquals(np.exp(- sigma ** 2 / 2), computed)
 
 # initialize the test suite
 loader = unittest.TestLoader()
-suite  = unittest.TestSuite()
+suite = unittest.TestSuite()
 
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromTestCase(TestServiceUnit))
-#suite.addTests(loader.loadTestsFromModule(scenario))
-#suite.addTests(loader.loadTestsFromModule(thing))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
-
-
