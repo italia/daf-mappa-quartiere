0188f8b164825082450f431fe50bc8fb0fba40fa
esterpantaleo <esterpantaleo@gmail.com>
2018-08-03 19:46:15 +0200

    modifieng dashboard

--- src/visualization/src/Dashboard.js
+++ src/visualization/src/Dashboard.js
@@ -37,7 +37,6 @@ class Dashboard extends Component {
 		</div>
 	    );
 	}
-	var piechartsY = this.props.dashboard.piechart.length * 100;
 	
 	//to do add IDquartiere in Milano_dashboard.json
 	var properties = this.props.neighborhood;
@@ -46,67 +45,8 @@ class Dashboard extends Component {
 	        <div>
 		    <h3 style={{textAlign: 'left'}}>
 		        {properties[this.props.nameField]}
-		    </h3>
-		    <ul style={{textAlign: 'left', padding: 0}}>
-	                {
-		            this.props.dashboard.list
-				.map(l => {
-				    
-				    return (
-					    <li>
-					        {l.label} : {sigFigs(this.props.dashboard.data[this.props.neighborhood.IDquartiere-1][l.field], 2)}
-				            </li>
-				    );
-	                        })
-			}
-		    </ul>
+	            </h3>
 		</div>
-		
-		<svg width='445' height='950'>
-	            {
-		        this.props.dashboard.barchart2
-			    .map((d, i) => {
-				var data1 = {
-				    values: d.data1.fields.map(f => this.props.dashboard.data[this.props.neighborhood.IDquartiere-1][f]),
-				    color: d.data1.color,
-				    label: d.data1.label
-				};
-				var data2 = {
-				    values: d.data2.fields.map(f => this.props.dashboard.data[this.props.neighborhood.IDquartiere-1][f]),
-				    color: d.data2.color,
-				    label: d.data2.label
-				};
-				return (
-					<BarChart2WithLegend
-				            key={i + "bar"}
-				            y={20 + i*200}
-				            title={d.label}
-				            data1={data1}
-				            data2={data2}
-				            labels={d.labels}
-					    dataSource={this.props.dashboard.dataSource}
-					/>
-				)})
-		    }
-	            {
-                        this.props.dashboard.piechart
-                            .map((p, i) => {
-                                var values = p.fields.map(f => this.props.dashboard.data[this.props.neighborhood.IDquartiere-1][f]);
-				
-                                return (
-                                        <PieChartWithLegend
-                                            key={i}
-                                            y={350 + i*200 + 15}
-                                            title={p.label}
-                                            labels={p.labels}
-                                            values={values}
-                                            colors={p.colors}
-                                            dataSource={this.props.dashboard.dataSource}
-                                         />
-                                )})
-                    }
-
-		</svg>
 	    </div>
 	);
     };
